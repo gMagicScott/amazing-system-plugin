985ae69f74594cd8d1f58fba7be9f417eeb1406b
Scott Lesovic <scott@guilefulmagic.com>
2013-05-24 13:13:49 -0400

    Add release 0.1.9

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ Plugin Name: Amazing System
 Plugin URI: http://www.amazingsystemtraining.com
 Description: Amazing System 3.0 (That thing from Dave Farr) - now for WordPress
 Author: Scott Lesovic
-Version: 0.1.8
+Version: 0.1.9
 Author URI: http://www.scottlesovic.com
 */
 
@@ -27,7 +27,7 @@ function amazing_system_installer() {
 	global $wp_rewrite;
 	$wp_rewrite->flush_rules();
 }
-
+register_activation_hook( __FILE__, 'amazing_system_installer' );
 
 /**
 * Register custom upgrades (non-WordPress hosted plugins)
--- readme.txt
+++ readme.txt
@@ -1,7 +1,7 @@
 === Amazing System ===
 Requires at least: 3.2
 Tested up to: 3.4.2
-Stable tag: 0.1.8
+Stable tag: 0.1.9
 
 Amazing System 3.0 (That thing from Dave Farr) - now for WordPress
 

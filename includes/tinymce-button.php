a526261c390d787c003257a6b94f9446114a4b59
Scott Lesovic <scott@scottlesovic.com>
2014-12-19 19:45:00 -0500

    Fix email link
    Previously the email link was shortening first and last names to just
    'Name'. Now, 'Name' expands to 'mc-firstname' and 'mc-lastname'.
    
    Special consideration was required for backwards compatibility for
    those who were using 'mc-firstname' in the shortcode as opposed to
    just 'firstname'. The same applies to 'mc-lastname'.
    
    [Addresses: #25]

--- includes/tinymce-button.php
+++ includes/tinymce-button.php
@@ -77,9 +77,23 @@ function amsys_link_generator ( $post ) {
 		do_shortcode( $the_extra_js );
 
 		foreach ($amsys_shortcode_fields as $key => $value ) {
-			if ( 'firstname' === $value || 'mc-firstname' === $value || 'lastname' === $value || 'mc-lastname' === $value ) {
-				$amsys_shortcode_fields[] = 'Name';
+			if ( 'Name' === $value ) {
+				$amsys_shortcode_fields[] = 'mc-firstname';
+				$amsys_shortcode_fields[] = 'mc-lastname';
 				unset( $amsys_shortcode_fields[ $key ] );
+				continue;
+			}
+
+			if ( 'firstname' === $value ) {
+				$amsys_shortcode_fields[] = 'mc-firstname';
+				unset( $amsys_shortcode_fields[ $key ] );
+				continue;
+			}
+
+			if ( 'lastname' === $value ) {
+				$amsys_shortcode_fields[] = 'mc-lastname';
+				unset( $amsys_shortcode_fields[ $key ] );
+				continue;
 			}
 		}
 		$amsys_shortcode_fields = array_unique( $amsys_shortcode_fields);
@@ -136,9 +150,15 @@ function amsys_get_1shop_code( $field ) {
 		case 'Workphone':
 			return '%$phone$%';
 			break;
-		case 'Name':
 		case 'firstname':
+		case 'mc-firstname':
+			return '%$firstname$%';
+			break;
 		case 'lastname':
+		case 'mc-lastname':
+			return '%$lastname$%';
+			break;
+		case 'Name':
 		case 'Company':
 		case 'Fax':
 		case 'Address1':
